5ff3d4e76ad7e5d51f275fb2d5d7d0bf7ec05612
SegFault42 <SegFault42@protonmail.com>
2019-04-10 20:26:56 +0700

    add reboot to payload

--- source/main.c
+++ source/main.c
@@ -1,26 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <math.h>
-
-#include <switch.h>
+#include "common.h"
+#include "print.h"
+#include "reboot.h"
 
 #define BUFF_SIZE (1024 * 16)
-
-typedef void (*ptrProgressBar)(double, double);
-
-void	printHeader(void)
-{
-	printf("\x1b[0;0H");
-	printf("================================================================================");
-	printf("%s%55s%s", CONSOLE_YELLOW, "Twili Disabler (By SegFault42)\n", CONSOLE_RESET);
-	printf("================================================================================");
-}
-
 
 bool	isFileExist(const char *file)
 {
@@ -36,77 +18,6 @@ bool	isFileExist(const char *file)
 bool	getTwiliState(void)
 {
 	return (isFileExist("sdmc:/atmosphere/titles/0100000000006480/flags/boot2.flag"));
-}
-
-void	drawButton(char *string, char *color)
-{
-	printf("\x1b[20;0H");
-
-	printf("%31s ________________\n", "");
-	printf("%31s|                |\n", "");
-	printf("%31s|                |\n", "");
-	printf("%31s|%s%16s%s|\n", "", color, string, CONSOLE_RESET);
-	printf("%31s|                |\n", "");
-	printf("%31s|                |\n", "");
-	printf("%31s ----------------\n", "");
-}
-
-void	warningMessage(char *str)
-{
-	int	align = 0;
-	u64	kDown = 0;
-
-	// align to vertical center
-	if (strlen(str) < 80) {
-		align = 40 - (strlen(str) / 2);
-	}
-
-	while (1) {
-		hidScanInput();
-		kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
-		printf("\x1b[25;%dH", align);
-
-		printf("%s%s%s", CONSOLE_RED, str, CONSOLE_RESET);
-
-		printf("\x1b[27;32HPush + to exit !");
-
-		if (kDown & KEY_PLUS) {
-			break ;
-		}
-		consoleUpdate(NULL);
-	}
-}
-
-void	progressBar(double TotalToDownload, double NowDownloaded)
-{
-	int		totaldotz=70;
-	double	fractiondownloaded = NowDownloaded / TotalToDownload;
-	int		dotz = round(fractiondownloaded * totaldotz);
-	int		meter=0;
-
-	// ensure that the file to be downloaded is not empty
-	// because that would cause a division by zero error later on
-	if (TotalToDownload <= 0.0) {
-		return ;
-	}
-
-	printf("%3.0f%% [",fractiondownloaded*100);
-
-	// part  that's full already
-	for ( ; meter < dotz; meter++) {
-		printf("=");
-	}
-
-	// remaining part (spaces)
-	for ( ; meter < totaldotz; meter++) {
-		printf(" ");
-	}
-
-	// and back to line begin
-	printf("]\r");
-
-	return ;
 }
 
 bool	copyFile(const char *dest, const char *src, ptrProgressBar progress)
@@ -264,8 +175,12 @@ int main(void)
 
 		printf("\x1b[50;66H");
 		printf("Press + to %sExit%s", CONSOLE_RED, CONSOLE_RESET);
-		if (kDown & KEY_PLUS)
+		if (kDown & KEY_PLUS) {
 			break;
+		}
+		if (kDown & KEY_MINUS) {
+			reboottopayload("hekate_ctcaer_4.9.1.bin");
+		}
 
 		consoleUpdate(NULL);
 	}
